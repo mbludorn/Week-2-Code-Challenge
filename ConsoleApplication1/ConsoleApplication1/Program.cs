7413efdacc18f917eff91904a3b3a3e13966282a
SeedPaths-User <lessdismissive@email.com>
2014-09-08 02:35:01 -0600

    Week 2 Code Chanllenge

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Hangman
+    {
+        //Below are the member variables for the class Hangman
+
+        public static int noOfLives = 7;
+        public static List<string> wordBank = new List<string>() 
+        {"apple", "nickeback", "computer", "seedpaths", 
+        "mountain", "sky", "river", "chair", "boy", "afternoon", "watermelon", "television", "bus", "water", "glass"};
+        public static string randomWord;
+        public static Random rng = new Random();
+        public static string lettersGuessed = string.Empty;
+        public static string usersGuess;
+        public static bool won = false;
+        public static string userName;
+
+        static void Main(string[] args)
+        {
+            //generated a random word
+            randomWord = wordBank[rng.Next(1, 16)];
+
+            //Geeting the user and explaing the rules by calling below function
+            Greet();
+
+            //Playing the game. 
+            //Divided each task into it's own function, so that code will be easy to manage and main looks clean
+            PlayGame();
+        }
+
+
+
+
+        //function to greet the user and display rules
+        static void Greet()
+        {
+            Console.WriteLine("Enter your name");
+            userName = Console.ReadLine();
+
+            Console.WriteLine("\nWelcome to Hangman game: " + userName);
+            Console.WriteLine("\nHandman Rules:\nYou need to guess the whole word or a single letter in the word");
+            Console.WriteLine("You will win, if you guess the whole word or all the letters in the word");
+            Console.WriteLine("You only have " + noOfLives + " lives to guess");
+            Console.WriteLine("If you can't guess in " + noOfLives + " lives, you will lose");
+            Console.WriteLine("Good luck!");
+        }
+
+
+        //function to display Masked word
+        static int MaskedWord(string randomWord)
+        {
+            int count = 0;
+            for (int i = 0; i < randomWord.Length; i++)
+            {
+                if (lettersGuessed.Contains(randomWord[i]))
+                {
+                    Console.Write(randomWord[i]);
+                }
+                else
+                {
+                    Console.Write(" _ ");
+                    count++;
+                }
+            }
+            return count;
+
+        }
+
+        //function contains the logic for the game
+        static void PlayGame()
+        {
+            int count;
+
+            while (won != true)
+            {
+
+                count = MaskedWord(randomWord);
+                if (count == 0)
+                {
+                    Console.WriteLine("\nCongratulations, " + userName + " you won!");
+                    Console.WriteLine("The word is: " + randomWord);
+                    won = true;
+                }
+
+                else
+                {
+                    //get user's guess
+                    Console.WriteLine("\n\nYou have " + noOfLives + " lives left. Guess");
+                    usersGuess = Console.ReadLine();
+                    UsersGuessVerify(usersGuess);
+                    if ((noOfLives == 0) && (won == false))
+                    {
+                        Console.WriteLine("\nSorry " + userName + " you ran out of lives. Game over");
+                        won = true;
+                    }
+                }
+
+            }
+
+        }
+
+        //function to verify users guess
+        static void UsersGuessVerify(string guess)
+        {
+            //verify if user entered a letter or word
+            if (guess.Length > 1) //means user entered a word
+            {
+                if (guess == randomWord)
+                {
+                    Console.WriteLine("Congratulations, " + userName + " you won!");
+                    //Console.WriteLine(randomWord);
+                    won = true;
+                    noOfLives = 0;
+                    return;
+                }
+                else
+                {
+                    noOfLives--;
+                    Console.WriteLine("You guessed wrong. You have " + noOfLives + " lives left");
+                    Console.WriteLine("You have guessed: " + lettersGuessed);
+                    return;
+                }
+            }
+            else //he entered a letter
+            {
+                //add that letter to the guessed letters
+                lettersGuessed = lettersGuessed + guess;
+
+                //verify if the word to be guessed has the letter entered by user
+                if (randomWord.Contains(guess))
+                {
+                    return;
+                }
+                else
+                {
+                    noOfLives--;
+                    Console.WriteLine("You guessed wrong. You have " + noOfLives + " lives left");
+                    Console.WriteLine("You have guessed: " + lettersGuessed);
+                    return;
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ Week 2 Code Challenge/Week 2 Code Challenge/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Code_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i <= 20; i = i + 1)
+            {
+                //Call the Fizzbuzz Function
+                Fizzbuzz(i);
+            }
+
+                //Call the LetterCounter Function using the name and the different parameters
+                LetterCounter('i', "I love biscuits and gravy. It's the best breakfast ever.");
+                
+                LetterCounter('n', "Never gonna give up. Never gonna let you down.");
+                
+                LetterCounter('s', "Sally sells seashells by the seashore. She's from Sussex.");
+
+            //Keep window open
+            Console.ReadKey();
+        }
+
+        static void Fizzbuzz(int number)
+        {
+            //If number is divisible by 5 and 3 then print fizzbuzz
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                Console.WriteLine("Fizzbuzz");
+            }
+            //If number is divisible by 5 print fizz
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            //If number is divisible by 3 print buzz 
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            //If not divisible by either 5 or 3 then print out the number
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void LetterCounter(char letter, string inString)
+         {
+            //declare variables to count the number of uppercase and lowercase letters
+            int numUpper = 0;
+            int numLower = 0;
+            int totalNum = numUpper + numLower;
+        
+        for (int i = 0; i < inString.Length; i++)
+        {
+            if (inString[i] ==letter) 
+            numLower++;
+            if (inString[i] == letter)
+            numUpper++;
+        }
+        
+
+            //Print to the Console
+             Console.WriteLine("Input:" + inString);
+             Console.WriteLine("Number of lowercase letters is " + numLower);
+             Console.WriteLine("Number of uppercase letters is " + numUpper);
+             Console.WriteLine("Total number of letters is " + totalNum);
+        }
+       }
+    }
